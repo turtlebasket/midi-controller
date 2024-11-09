f5900d11e3254af3cc4e869a4cfb3ce94875bea9
turtlebasket <turtlebasket@gmail.com>
2024-11-09 14:43:17 -0800

    build changes

--- firmware/main/build.zig
+++ firmware/main/build.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
         .name = "zig",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
